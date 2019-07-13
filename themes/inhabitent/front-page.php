9528bc9cef944be8a224b8308738fe7edf25d74e
roberto0412 <roberto_esquivel04@outlook.com>
2019-07-13 14:49:58 -0700

    finished stlying landing page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -9,32 +9,16 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<div class="logo-tent">
 
-		<?php if ( have_posts() ) : ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+			<img src="<?php echo get_template_directory_uri(); ?>/project-04/images/logos/inhabitent-logo-full.svg" width="260px" height="260px" alt="" />
 
-			<?php endwhile; ?>
+			</div>
 
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -18,19 +18,39 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-		<div id="page" class="hfeed site">
-			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
+			<div id="page" class="hfeed site">
+				<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
+				
+				<header id="masthead" class="site-header" role="banner">
 
-			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div><!-- .site-branding -->
-
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					
+					
+					
+					
+					<nav id="site-navigation" class="main-navigation" role="navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+						
+						
+						<div class="containerr">
+						
+						<div class="logo">
+							
+							<img class="logoo" src="<?php echo get_template_directory_uri(); ?>/project-04/images/logos/inhabitent-logo-tent.svg" alt="">
+	
+	
+					   </div>
+					
+						
+		
+					
+					
+					</div>
 				</nav><!-- #site-navigation -->
+				
+
+
+
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">

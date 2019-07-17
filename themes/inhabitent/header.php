b433b39a2bdc1aeb0ab452f730980b5379dfd81c
roberto0412 <roberto_esquivel04@outlook.com>
2019-07-16 18:59:59 -0700

    home, page styling and about page styles updated

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -18,39 +18,25 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-			<div id="page" class="hfeed site">
-				<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
-				
-				<header id="masthead" class="site-header" role="banner">
+		<div id="page" class="hfeed site">
+			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
-					
-					
-					
-					
-					<nav id="site-navigation" class="main-navigation" role="navigation">
-						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-						
-						
-						<div class="containerr">
-						
-						<div class="logo">
-							
-							<img class="logoo" src="<?php echo get_template_directory_uri(); ?>/project-04/images/logos/inhabitent-logo-tent.svg" alt="">
-	
-	
-					   </div>
-					
-						
-		
-					
-					
-					</div>
+			<header id="masthead" class="site-header" role="banner">
+
+
+			<div class="container">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"> 
+				<img class="logo_tent"src="<?php echo get_template_directory_uri(); ?>/project-04/images/logos/inhabitent-logo-tent.svg" width="260px" height="260px" alt="" />
+
+				</a>
+
+
+
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
-				
-
-
-
+             </div>
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
--- themes/inhabitent/page-about.php
+++ themes/inhabitent/page-about.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area content-about">
+	<div id="primary" class="about_background">
 		<main id="main" class="site-main" role="main">
 		<h1>About</h1>
 
@@ -20,5 +20,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
